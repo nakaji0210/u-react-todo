f7dc8d392d9608669bd5b817503732d0800fc5c3
Hiroyuki Nakajima <70434487+nakaji0210@users.noreply.github.com>
2021-11-06 11:56:29 +0900

    stateを定義して追加ボタンの挙動を実装

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,33 @@ import "./styles.css";
 
 export const App = () => {
   // stateを定義
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
   const [completeTodos, setCompleteTodos] = useState(["ううう", "えええ"]);
+
+  // inputタグでのstate変更をonCangeで受け取るように定義(eventは仮引数)
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  // 追加ボタンのアクションを定義
+  const onClickAddTodo = () => {
+    // 空の場合はそのままreturn
+    if (todoText === "") return;
+    // スプレッド構文で未完了TODOの配列を展開しつつinputタグの値を追加
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    // 追加後はinputエリアに空文字をセットしてクリアする
+    setTodoText("");
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          value={todoText}
+          onChange={onChangeTodoText}
+          placeholder="TODOを入力"
+        />
+        <button onClick={onClickAddTodo}>追加</button>
       </div>
 
       <div className="incomplete-area">
